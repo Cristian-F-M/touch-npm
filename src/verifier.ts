8fc70b77d46ce06210aef538e16ed5189c7c5fef
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-04 16:59:01 -0500

    Add function to validate structure

--- src/verifier.ts
+++ src/verifier.ts
@@ -1,10 +1,44 @@
 import {
 	CLOSE_SYMBOLS,
 	EXTENSION_SEPARATORS,
+	OPEN_CLOSE_PAIRS,
 	OPEN_SYMBOLS,
 	SEPARATOR_SYMBOLS,
 } from '@/constants/symbols'
 import type { CloseSymbol, OpenSymbol, Token } from '@/types/index.d.ts'
+
+export function validateStructure(input: string) {
+	const stack: string[] = []
+	const pairs: Record<string, string> = {
+		'{': '}',
+		'[': ']',
+		'(': ')',
+	}
+
+	for (let i = 0; i < input.length; i++) {
+		const char = input[i]!
+
+		if (pairs[char]) {
+			stack.push(char)
+			continue
+		}
+
+		if (Object.values(pairs).includes(char)) {
+			const lastStack = stack.pop()
+			if (!lastStack || pairs[lastStack] !== char)
+				return { valid: false, error: `Unexpected '${char}' at position ${i}` }
+		}
+	}
+
+	if (stack.length) {
+		return {
+			valid: false,
+			error: `Missing closing '${pairs[stack.at(-1)!]}'`,
+		}
+	}
+
+	return { valid: true }
+}
 
 function tokenize(input: string) {
 	const tokens: Token[] = []
