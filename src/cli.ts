746eb30f9df4a7b8edf1a2d10e2ecd83704baad0
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-08 16:00:43 -0500

    Add help message and logic to show it

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,15 @@
 import { build } from '@/builder'
+import { HELP_MESSAGE } from '@/constants/help'
 import { error } from '@/logger'
 import { getExtension, parse } from '@/parser'
 import { validateInput } from '@/verifier'
 
 const args = process.argv.slice(2)
 
-if (!args.length) process.exit(1)
+if (!args.length || args.includes('--help') || args.includes('-h')) {
+	console.log(HELP_MESSAGE)
+	process.exit(0)
+}
 
 const input = args[0]!.replace(/\s/g, '')
 const validation = validateInput(input)
---
+++ src/constants/help.ts
@@ -0,0 +1,26 @@
+import { version } from '../../package.json'
+
+export const HELP_MESSAGE = `
+@cmorales/touch v${version}
+Creating complex file structures has never been easier.
+
+Usage:
+  touch <input>     Generate file structure    (e.g. touch src/{components,utils})
+  touch --help      Show this help message     (e.g. touch --help)
+  touch -h          Show this help message     (e.g. touch -h)
+
+Features:
+  Directories       Append '/' to name         (e.g. app/models/)
+  Grouping          Use {}, [], or ()          (e.g. src/{components,utils})
+  Nesting           Nest groups for complexity (e.g. src/{components/{Button,Input},utils})
+  Extensions        Per file or default        (e.g. src/{index.ts,styles.css} or src/{a,b}.ts)
+
+Author:
+  Cristian Morales (@cmorales)
+  GitHub: https://github.com/Cristian-F-M
+  Twitter: https://x.com/Morales_M20
+
+Support:
+  If you find this useful, support my work:
+  https://www.buymeacoffee.com/cmorales
+`
